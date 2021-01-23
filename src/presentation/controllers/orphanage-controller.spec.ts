84e4c13cdb142ca5c62e015b451568c8b405d2e4
Felipe Almeida <felipethiago27@gmail.com>
2021-01-23 14:26:59 -0300

    test: ensure OrphanageController returns 400 is no open_on_weekends is provided

--- src/presentation/controllers/orphanage-controller.spec.ts
+++ src/presentation/controllers/orphanage-controller.spec.ts
@@ -91,4 +91,12 @@ describe('OrphanageController', () => {
     const httResponse = sut.handle(httpRequest)
     expect(httResponse.statusCode).toBe(400)
   })
+
+  test('Should return 400 if no open_on_weekends is provided', () => {
+    const sut = new OrphanageController()
+    const httpRequest = mockHttpRequest()
+    delete httpRequest.body.open_on_weekends
+    const httResponse = sut.handle(httpRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
 })
