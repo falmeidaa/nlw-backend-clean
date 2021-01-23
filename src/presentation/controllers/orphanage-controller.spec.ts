68711dc721dfeb70c2f96cea49d79593032be591
Felipe Almeida <felipethiago27@gmail.com>
2021-01-23 14:22:48 -0300

    test: ensure OrphanageController returns 400 is no latitude is provided

--- src/presentation/controllers/orphanage-controller.spec.ts
+++ src/presentation/controllers/orphanage-controller.spec.ts
@@ -1,17 +1,62 @@
 import faker from 'faker'
 
+type HttpResponse = {
+  statusCode: number
+  body: any
+}
+
+type HttpRequest = {
+  body?: any
+}
+
 class OrphanageController {
-  handle (httpRequest: any): any {
-    return ({
-      statusCode: 400
-    })
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const fields = [
+      'name',
+      'latitude',
+      'longitude',
+      'about',
+      'instructions',
+      'opening_hours',
+      'open_on_weekends'
+    ]
+    for (const field of fields) {
+      if (!httpRequest.body[field]) {
+        return ({
+          statusCode: 400,
+          body: httpRequest.body
+        })
+      }
+    }
   }
 }
+
+const mockHttpRequest = (): HttpRequest => ({
+  body: {
+    name: faker.name.findName(),
+    latitude: faker.random.number(),
+    longitude: faker.random.number(),
+    about: faker.random.words(),
+    instructions: faker.random.words(),
+    opening_hours: faker.random.words(),
+    open_on_weekends: faker.random.boolean()
+  }
+})
 
 describe('OrphanageController', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new OrphanageController()
-    const httResponse = sut.handle(faker.random.objectElement())
+    const httpRequest = mockHttpRequest()
+    delete httpRequest.body.name
+    const httResponse = sut.handle(httpRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no latitude is provided', () => {
+    const sut = new OrphanageController()
+    const httpRequest = mockHttpRequest()
+    delete httpRequest.body.latitude
+    const httResponse = sut.handle(httpRequest)
     expect(httResponse.statusCode).toBe(400)
   })
 })
