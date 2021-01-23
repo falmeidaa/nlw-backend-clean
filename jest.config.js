d57adda1d3c4123d2a2f9769f489698a061e76a5
Felipe Almeida <felipethiago27@gmail.com>
2021-01-23 12:13:02 -0300

    refactor: change standard configs

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
