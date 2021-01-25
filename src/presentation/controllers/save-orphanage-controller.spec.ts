4a0eb2385dc042c4545a5b11779ec53fe065e428
Felipe Almeida <felipethiago27@gmail.com>
2021-01-25 14:27:08 -0300

    test: ensure SaveOrphanageController retruns 200 if valid data is provided

--- src/presentation/controllers/save-orphanage-controller.spec.ts
+++ src/presentation/controllers/save-orphanage-controller.spec.ts
@@ -1,8 +1,12 @@
 import faker from 'faker'
+import { OrphanageModel } from '../../domain/models/orphanage'
+import { SaveOrphanage, SaveOrphanageModel } from '../../domain/usecases/save-orphanage'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest, ok } from '../helpers/http-helper'
 import { HttpRequest } from '../protocols/http/http'
 import { SaveOrphanageController } from './save-orphanage-controller'
 
-const mockHttpRequest = (): HttpRequest => ({
+const mockHttpRequest = (): HttpRequest<SaveOrphanageModel> => ({
   body: {
     name: faker.name.findName(),
     latitude: faker.random.number(),
@@ -14,60 +18,97 @@ const mockHttpRequest = (): HttpRequest => ({
   }
 })
 
+class SaveOrphanageSpy implements SaveOrphanage {
+  params: SaveOrphanageModel
+  async save (orphanage: SaveOrphanageModel): Promise<OrphanageModel> {
+    this.params = orphanage
+    const orphanageModel = Object.assign(orphanage, {}, { id: 'any_id' })
+    return Promise.resolve(orphanageModel)
+  }
+}
+
+type SutTypes = {
+  sut: SaveOrphanageController
+  saveOrphanageSpy: SaveOrphanageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const saveOrphanageSpy = new SaveOrphanageSpy()
+  const sut = new SaveOrphanageController(saveOrphanageSpy)
+  return {
+    sut,
+    saveOrphanageSpy
+  }
+}
+
 describe('OrphanageController', () => {
-  test('Should return 400 if no name is provided', () => {
-    const sut = new SaveOrphanageController()
+  test('Should return 400 if no name is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = mockHttpRequest()
     delete httpRequest.body.name
-    const httResponse = sut.handle(httpRequest)
-    expect(httResponse.statusCode).toBe(400)
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual(badRequest(new MissingParamError('name')))
   })
 
-  test('Should return 400 if no latitude is provided', () => {
-    const sut = new SaveOrphanageController()
+  test('Should return 400 if no latitude is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = mockHttpRequest()
     delete httpRequest.body.latitude
-    const httResponse = sut.handle(httpRequest)
-    expect(httResponse.statusCode).toBe(400)
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual(badRequest(new MissingParamError('latitude')))
   })
 
-  test('Should return 400 if no longitude is provided', () => {
-    const sut = new SaveOrphanageController()
+  test('Should return 400 if no longitude is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = mockHttpRequest()
     delete httpRequest.body.longitude
-    const httResponse = sut.handle(httpRequest)
-    expect(httResponse.statusCode).toBe(400)
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual(badRequest(new MissingParamError('longitude')))
   })
 
-  test('Should return 400 if no about is provided', () => {
-    const sut = new SaveOrphanageController()
+  test('Should return 400 if no about is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = mockHttpRequest()
     delete httpRequest.body.about
-    const httResponse = sut.handle(httpRequest)
-    expect(httResponse.statusCode).toBe(400)
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual(badRequest(new MissingParamError('about')))
   })
 
-  test('Should return 400 if no instructions is provided', () => {
-    const sut = new SaveOrphanageController()
+  test('Should return 400 if no instructions is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = mockHttpRequest()
     delete httpRequest.body.instructions
-    const httResponse = sut.handle(httpRequest)
-    expect(httResponse.statusCode).toBe(400)
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual(badRequest(new MissingParamError('instructions')))
   })
 
-  test('Should return 400 if no opening_hours is provided', () => {
-    const sut = new SaveOrphanageController()
+  test('Should return 400 if no opening_hours is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = mockHttpRequest()
     delete httpRequest.body.opening_hours
-    const httResponse = sut.handle(httpRequest)
-    expect(httResponse.statusCode).toBe(400)
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual(badRequest(new MissingParamError('opening_hours')))
   })
 
-  test('Should return 400 if no open_on_weekends is provided', () => {
-    const sut = new SaveOrphanageController()
+  test('Should return 400 if no open_on_weekends is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = mockHttpRequest()
     delete httpRequest.body.open_on_weekends
-    const httResponse = sut.handle(httpRequest)
-    expect(httResponse.statusCode).toBe(400)
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse).toEqual(badRequest(new MissingParamError('open_on_weekends')))
+  })
+
+  test('Should call SaveOrphanage with correct values', async () => {
+    const { sut, saveOrphanageSpy } = makeSut()
+    const httpRequest = mockHttpRequest()
+    await sut.handle(httpRequest)
+    expect(saveOrphanageSpy.params).toEqual(httpRequest.body)
+  })
+
+  test('Should return a OrphanageModel on success', async () => {
+    const { sut } = makeSut()
+    const httpRequest = mockHttpRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(ok(Object.assign(httpRequest.body, {}, { id: 'any_id' })))
   })
 })
