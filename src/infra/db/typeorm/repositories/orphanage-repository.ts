94c4a97f5ad3e790ea335b396868ea17d229291b
Felipe Almeida <felipethiago27@gmail.com>
2021-01-26 10:28:45 -0300

    refactor: rename class to TypeORMRepository

--- src/infra/db/typeorm/repositories/orphanage-repository.ts
+++ src/infra/db/typeorm/repositories/orphanage-repository.ts
@@ -4,7 +4,7 @@ import { SaveOrphanageRepository } from '@/data/protocols/save-orphanage-reposit
 import { Orphanage } from '@/typeorm/entities/orphanage'
 import TypeOrmHelper from '../helpers/typeorm-helper'
 
-export class OrphanageTypeormRepository implements SaveOrphanageRepository {
+export class OrphanageTypeORMRepository implements SaveOrphanageRepository {
   async save (orphanageData: SaveOrphanageModel): Promise<OrphanageModel> {
     const orphanageRepository = await TypeOrmHelper.getEntityRepository(Orphanage)
     const orphanage = await orphanageRepository.save(orphanageData)
