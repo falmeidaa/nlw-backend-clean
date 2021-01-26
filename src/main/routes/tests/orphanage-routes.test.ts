e2928e2baa7687dbd62c4871f03ecbda81078115
Felipe Almeida <felipethiago27@gmail.com>
2021-01-26 14:59:04 -0300

    test: ensure Orphanage route returns an orphanage on success

---
+++ src/main/routes/tests/orphanage-routes.test.ts
@@ -0,0 +1,34 @@
+import TypeOrmHelper from '@/infra/db/typeorm/helpers/typeorm-helper'
+import app from '@/main/config/app'
+import request from 'supertest'
+import faker from 'faker'
+
+const orphanage = {
+  name: faker.name.findName(),
+  latitude: faker.random.number(),
+  longitude: faker.random.number(),
+  about: faker.random.words(),
+  instructions: faker.random.words(),
+  opening_hours: faker.random.words(),
+  open_on_weekends: faker.random.boolean()
+}
+
+describe('Orphanage Routes', () => {
+  beforeAll(async () => {
+    await TypeOrmHelper.create('test')
+  })
+
+  beforeEach(() => {
+    TypeOrmHelper.clear()
+  })
+
+  afterAll(async () => {
+    await TypeOrmHelper.close()
+  })
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/Orphanage')
+      .send(orphanage)
+      .expect(200)
+  })
+})
