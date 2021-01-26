db0945c68ab94fb0391efb75fc3e3d68f8ba1687
Felipe Almeida <felipethiago27@gmail.com>
2021-01-26 15:20:28 -0300

    feat: add SaveOrphanage factory

---
+++ src/main/factoires/save-orphanage-factory.ts
@@ -0,0 +1,7 @@
+import { OrphanageTypeORMRepository } from '@/infra/db/typeorm/repositories/orphanage-repository'
+import { SaveOrphanageController } from '@/presentation/controllers/save-orphanage-controller'
+
+export const makeSaveOrphanageController = (): SaveOrphanageController => {
+  const dbSaveOrphanage = new OrphanageTypeORMRepository()
+  return new SaveOrphanageController(dbSaveOrphanage)
+}
